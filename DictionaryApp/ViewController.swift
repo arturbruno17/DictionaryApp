b1e4b7b2d69ad04fbdf89e2a78326ddc649b603e
Artur Bruno <arturjuliao20@gmail.com>
2026-02-07 16:20:27 -0300

    Initial Commit

---
+++ DictionaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DictionaryApp
+//
+//  Created by Artur Bruno on 07/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
